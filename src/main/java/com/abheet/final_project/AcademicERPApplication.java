897572dcb3aff2d4c6f7eabccbf8c2cca365c6e0
Abheet Sethi <abheeet.sethi@gmail.com>
2024-11-26 12:06:06 +0530

    Initial commit

---
+++ src/main/java/com/abheet/final_project/AcademicERPApplication.java
@@ -0,0 +1,11 @@
+package com.abheet.final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicErpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicErpApplication.class, args);
+    }
+}
